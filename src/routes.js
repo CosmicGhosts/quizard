793751c91c988d6ccb388020bcbe5a24d87e9c66
seanstrom <sean@seanhagstrom.com>
2015-08-25 15:32:22 -0700

    render homepage

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var router  = express.Router()
 
-router.get('/', function (req, res) {})
+router.get('/', function (req, res) {
+  res.render('index', {title: 'Behold! Quizard'})
+})
+
 module.exports = router
