2376d967d873c829a83e9853c31bffc007a7844d
seanstrom <sean@seanhagstrom.com>
2015-08-26 11:38:53 -0700

    initial user authentication integration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var path = require('path')
+var passport = require('passport')
 var routes = require('./routes')
+var models = require('./models')
 var app = express()
 
 app.use('/', routes)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(models.User.createStrategy())
+passport.serializeUser(models.User.serializeUser())
+passport.deserializeUser(models.User.deserializeUser())
 
 module.exports = app
