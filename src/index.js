588bad1e7c52792ecaee1b4ab41fd9f31c904300
seanstrom <sean@seanhagstrom.com>
2015-08-26 11:38:13 -0700

    integrate db into app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 var app = require('./app')
+var models = require('./models')
 var port = process.env.PORT || 3000
 
+function startApp () {
+  app.listen(port, function () {
+    console.log('started on port: ' + port)
+  })
+}
+
 app.set('port', port)
-app.listen(port, function () {
-  console.log('started on port: ' + port)
-})
+models.sequelize
+  .sync()
+  .then(startApp)
