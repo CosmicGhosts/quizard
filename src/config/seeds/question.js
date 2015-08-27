a2477bc79f7731f66c7cc2e04d7844b2390b429d
seanstrom <sean@seanhagstrom.com>
2015-08-27 14:50:04 -0700

    tweak seed for questions to have answers

--- src/config/seeds/question.js
+++ src/config/seeds/question.js
@@ -1,11 +1,27 @@
 var basePath = '../../'
 var models = require(basePath + 'models')
 var Question = models.Question
+var Answer = models.Answer
 
 function complete () { process.exit() }
 // models.sequelize.sync({force:true})
 
-Question
-  .findOrCreate({ where: { title: 'What is the Meaning of Life?' } })
-  .then(complete)
-  .catch(console.log)
+function addAnswers (questions) {
+  var answers = questions.map(function (question) {
+    return Answer.create({
+      description: 'Love',
+      QuestionId: question.id
+    })
+  })
+
+  return Promise.all(answers)
+}
+
+function createQuestion (question, answers) {
+  Question.findOrCreate({ where: { title: 'What is the Meaning of Life?' } })
+    .then(addAnswers)
+    .then(complete)
+    .catch(console.log)
+}
+
+createQuestion()
