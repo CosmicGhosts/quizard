2570a1f88cd934209f82db9afcedfdbbe7756474
seanstrom <sean@seanhagstrom.com>
2015-08-28 16:03:19 -0700

    throw away old tests

--- test/feature/featureHelper.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = 'test'
-var port = process.env.PORT = 4000
-var Browser = require('zombie')
-Browser.localhost('quizard.com', port)
-var browser = new Browser()
-
-exports.browser = browser
-exports.models = require('../../src/models')
-exports.app = require('../../src')
