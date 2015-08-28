3ce095e1ee53405b8527818e007f020711d09dec
seanstrom <sean@seanhagstrom.com>
2015-08-28 13:01:33 -0700

    add functionality around properly creating answers and questions
    also added functionality for displaying metrics around
    the amount of times answer was chosen

--- src/repos/questions.js
+++ src/repos/questions.js
@@ -36,11 +36,19 @@ function getQuestionWithAnswers () {
   return Question.findAll({ include: [Answer] })
 }
 
+function firstQuestion (questions) {
+  return questions[0]
+}
+
 function getUnansweredQuestion (answeredIds) {
-  var baseQuery = { include: [Answer] }
+  var baseQuery = {
+    include: [Answer],
+    order: [models.Sequelize.fn('Rand')]
+  }
   var query = buildUnansweredQuery(baseQuery, answeredIds)
   return Question
-    .findOne(query)
+    .findAll(query)
+    .then(firstQuestion)
     .then(nullQuestion)
 }
 
@@ -60,13 +68,50 @@ function getAnsweredQuestions (userToken) {
     include: [userAnswerInclude]
   }
 
-  return Question.findAll({
-    include: [answerInclude],
-    order: [models.Sequelize.fn('RAND')]
+  return Question.findAll({ include: [answerInclude] })
+}
+
+function createQuestion (data) {
+  return Question.create(data)
+}
+
+function getAnswerUserAnswerCount (answerId) {
+  return UserAnswer.findAndCountAll({
+    where: { 'AnswerId': answerId }
   })
 }
 
+function addUserAnswerCount (answer) {
+  return getAnswerUserAnswerCount(answer.id)
+    .then(function (countObj) {
+      answer.UserAnswerCount = countObj.count
+      return answer
+    })
+}
+
+function addAnswerUserAnswerCount (question) {
+  var pendingAnswers = question.Answers.map(addUserAnswerCount)
+  return Promise.all(pendingAnswers)
+    .then(function (answers) {
+      question.Answers = answers
+      return question
+    })
+}
+
+function getStats () {
+  var sequelize = models.sequelize
+  var attrs = Object.keys(Answer.attributes)
+  return Question
+    .findAll({ include: [Answer] })
+    .then(function (questions) {
+      var pendingQuestions = questions.map(addAnswerUserAnswerCount)
+      return Promise.all(pendingQuestions)
+    })
+}
+
+exports.create = createQuestion
 exports.getQAs = getQuestionWithAnswers
+exports.getStats = getStats
 exports.getAnsweredQuestions = getAnsweredQuestions
 exports.getUnansweredQuestion = getUnansweredQuestion
 exports.nullQuestion = nullQuestion
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,9 @@
 var helpers = require('./helpers')
 var models = helpers.models
 var User = models.User
+var Answer = models.Answer
 var Admin = models.Admin
+var Question = models.Question
 var QuestionsRepo = require('../repos/questions')
 
 var renderUsersView = helpers.render('users')
@@ -42,9 +44,9 @@ function getAdmins (req, res) {
     .then(renderElders(res))
 }
 
-function renderAdminDashboard (req, res) {
+function getQuestions (req, res) {
   QuestionsRepo.getQAs().then(function (questions) {
-    res.render('admin/dashboard', {
+    res.render('admin/questions', {
       questions: questions
     })
   })
@@ -54,10 +56,49 @@ function renderLogin (req, res) {
   res.render('admin/login')
 }
 
+function renderDashboard (req, res) {
+  res.render('admin/dashboard')
+}
+
+function createQuestion (req, res) {
+  var questionTitle = req.body.questionTitle
+  var questionDescription = req.body.questionDescription
+  var pendingQuestion = QuestionsRepo.create({
+    title: questionTitle,
+    description: questionDescription
+  })
+  return pendingQuestion
+    .then(function (question) {
+      res.redirect('/admin/questions')
+    })
+}
+
+function createAnswer (req, res) {
+  var questionId = req.params.questionId
+  var answerDescription = req.body.answerDescription
+  return Answer
+    .create({
+      description: answerDescription,
+      QuestionId: questionId
+    })
+    .then(function (answer) {
+      res.redirect('/admin/questions')
+    })
+}
+
+function getStats (req, res) {
+  return QuestionsRepo.getStats().then(function (questions) {
+    res.render('admin/dashboard', { questions: questions })
+  })
+}
+
 module.exports = function (app, passport) {
-  app.get('/admin', isLoggedIn, renderAdminDashboard)
+  app.get('/admin', isLoggedIn, getStats)
   app.get('/admin/login', renderLogin)
   app.get('/admin/users', isLoggedIn, getUsers)
-  app.get('/admin/elders', getAdmins)
+  app.get('/admin/elders', isLoggedIn, getAdmins)
+  app.get('/admin/questions', isLoggedIn, getQuestions)
   app.post('/admin/login', authLogin(passport))
+  app.post('/admin/questions/new', isLoggedIn, createQuestion)
+  app.post('/admin/questions/:questionId/answers/new', isLoggedIn, createAnswer)
 }
