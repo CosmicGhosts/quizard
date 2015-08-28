5da4be16b661bb9e2a4d439d77beb98b47d10cbb
seanstrom <sean@seanhagstrom.com>
2015-08-27 18:53:18 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 var app = require('./app')
 var models = require('./models')
 var port = process.env.PORT || 3000
-
-function startApp () {
-  var server = app.listen(port, function () {})
-  return server
-}
-
 app.set('port', port)
-var appSetup = models.sequelize
-  .sync()
-  .then(startApp)
-
-module.exports = appSetup
+module.exports = models.sequelize.sync()
+  .then(function () {
+    app.listen(port, function () {})
+  })
